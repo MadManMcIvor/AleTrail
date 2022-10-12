2105d72d0c321d795168dd7bdca38a38133430c8
Alex <mcivor444@gmail.com>
2022-10-12 15:31:55 -0700

    created brewery card modal

--- ghi/src/BreweryCard.js
+++ ghi/src/BreweryCard.js
@@ -1,5 +1,11 @@
+import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import BreweryCardModal from './BreweryCardModal';
 
 function BreweryCard(props) {
+    
+    const [modalShow, setModalShow] = useState(false);
+
 
     return (
         <div key={props.brewery_id}>
@@ -8,8 +14,23 @@ function BreweryCard(props) {
                 <div className="card-body">
                     <h5 className="card-title">{props.name}</h5>
                     <p className="card-text">{props.description}</p>
+                    <Button variant="primary" onClick={() => setModalShow(true)}>See more</Button>
                 </div>
             </div>  
+            <BreweryCardModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                brewery_id = {props.brewery_id}
+                name = {props.name}
+                street = {props.street}
+                city = {props.city}
+                state = {props.state}
+                zip_code = {props.zip_code}
+                phone = {props.phone}
+                image_url = {props.image_url}
+                description = {props.description}
+                website = {props.website}
+            />
         </div>
         
     )
