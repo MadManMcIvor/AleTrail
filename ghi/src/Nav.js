b46ed3e20a59fa4f12706a86d4b5770020fc4223
Alex <mcivor444@gmail.com>
2022-10-13 16:18:20 -0700

    reworked the navbar to add the logo and make it white themed

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -6,24 +6,34 @@ import Navbar from 'react-bootstrap/Navbar';
 function OurNav() {
   return (
     <>
-      <Navbar bg="warning" variant="light" expand="md">
-        <Container>
-          <Navbar.Brand href="/breweries">AleTrail</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <NavLink className="nav-link" to="/breweries">Breweries</NavLink>
-                <NavLink className="nav-link" to="/beers">Beers</NavLink>
-                <NavLink className="nav-link" to="/favorites">Favorites</NavLink>
-              </Nav>
-              <Nav>
-                <NavLink className="nav-link" to="/settings">Settings</NavLink>
-                <NavLink className="nav-link" to="/login">Sign Up</NavLink>
-                <NavLink className="nav-link" to="/login">Login</NavLink>
-                <NavLink className="nav-link" to="/login">Logout</NavLink>
-              </Nav>
-          </Navbar.Collapse>
-        </Container>
+      <Navbar bg="white" variant="light" expand="md">
+        <NavLink className="nav-link" to="/breweries">
+          <img
+                  src={require('./images/logo.png')}
+                  width="120"
+                  height="100"
+                  className="d-inline-block align-top"
+                  alt="AleTrail logo"
+                />
+          </NavLink>
+          <Container>
+            <Navbar.Brand href="/breweries">
+            </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <NavLink className="nav-link" to="/breweries"><h3>Breweries</h3></NavLink>
+                  <NavLink className="nav-link" to="/beers"><h3>Beers</h3></NavLink>
+                  <NavLink className="nav-link" to="/favorites"><h3>Favorites</h3></NavLink>
+                </Nav>
+                <Nav>
+                  <NavLink className="nav-link" to="/settings">Settings</NavLink>
+                  <NavLink className="nav-link" to="/login">Sign Up</NavLink>
+                  <NavLink className="nav-link" to="/login">Login</NavLink>
+                  <NavLink className="nav-link" to="/login">Logout</NavLink>
+                </Nav>
+            </Navbar.Collapse>
+          </Container>
       </Navbar>
     </>
   );
