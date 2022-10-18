08ddd2fefb19f836af5d86448bb9a3b3ab1b6db9
Alex <mcivor444@gmail.com>
2022-10-18 16:00:34 -0700

    initial commit for the favorites end points branch

--- users_and_favorites/main.py
+++ users_and_favorites/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from authenticator import authenticator
-from routers import users
+from routers import users, favorites_router
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(favorites_router.router)
 app.include_router(authenticator.router)
