c37139bc5572b41ca7662ec41172ddfaceed87ad
Justin Thomas <justintt5365@gmail.com>
2022-10-19 16:49:04 -0400

    created favoriteicon component and added it to brewerycardmodal

---
+++ ghi/src/FavoriteIcon.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+function FavoriteIcon(props) {
+
+    const [fav, setFav] = useState(false)
+
+    function RenderStar () {
+        if(fav === false){
+            return (
+                <img src="/empty-star-icon.png" width="50"></img>
+            )
+        }else{
+            return(
+                <img src="/star-icon.png" width="50"></img>
+            )
+        }
+    }
+
+    function updateFav(boolean){
+        if(fav === false){
+            setFav(true)
+        }else{
+            setFav(false)
+        }
+    }
+
+  return (
+    <div className='favorite' onClick={updateFav}>
+        <RenderStar></RenderStar>
+    </div>
+  );
+}
+
+export default FavoriteIcon;
