7f742c264212cc2b286c6c1e94b77772e780f849
Ryan <patsfanryan11@gmail.com>
2022-10-19 16:51:26 -0700

    Wrote my unit test, though it isn't working properly yet

---
+++ breweries_and_beers/tests/test_beers.py
@@ -0,0 +1,54 @@
+from fastapi.testclient import TestClient
+import sys
+import pathlib
+import os
+ 
+fastapi_dir = pathlib.Path(__file__).parent.parent.resolve()
+abs_dir = os.path.abspath(fastapi_dir)
+sys.path.append(abs_dir)
+
+from main import app
+from db import BeerQueries
+
+client = TestClient(app)
+
+class EmptyBeerQueries:
+    def get_beers(self):
+        return []
+
+class MockBeerQueries:
+    def get_beers(self):
+        return [beer]
+
+beer = {
+    "beer_id": 1,
+    "name": "str",
+    "description": "str",
+    "type": "str",
+    "ibu": "int",
+    "abv": 0,
+    "brewery": "int",
+    "image_url": "str",
+    "category": "str",
+    "vegetarian_friendly": "bool",
+}
+
+def test_get_beers_empty():
+    app.dependency_overrides[BeerQueries] = EmptyBeerQueries
+
+    response = client.get('/beers')
+
+    assert response.status_code == 200
+    assert response.json() == { 'beers': [] }
+
+    app.dependency_overrides = {}
+
+def test_get_beers():
+    app.dependency_overrides[BeerQueries] = MockBeerQueries
+
+    response = client.get('/beers')
+
+    assert response.status_code == 200
+    assert response.json() == { 'beers': [beer] }
+
+    app.dependency_overrides = {}
