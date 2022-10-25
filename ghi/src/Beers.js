7d3ff8b1556b2b33f09300fc289f44419ca3ee50
Justin Thomas <justintt5365@gmail.com>
2022-10-25 17:14:26 -0400

    updated beers.js to get fav beers and send fav data to child component

--- ghi/src/Beers.js
+++ ghi/src/Beers.js
@@ -5,21 +5,56 @@ import React, { useEffect, useState } from 'react';
 function Beers() {
     const [beers, setBeers] = useState([])
 
-
+    async function addFavsToBreweries(brew, favData) {
+      // creates a list of all brewery_ids in Favorite breweries
+      if(favData.length > 0){
+        let favs = [];
+        const user_id = favData[0]["user_id"]
+        for(let i = 0; i < favData.length; i++){
+          if(!(favs.includes(favData[i]["brewery_id"]))){
+            favs.push(favData[i]["brewery_id"]);
+          };
+        };
+        // compares brewery id to list of brew. ids from favorites and add true or false to object depending if in list or not
+        for(let i = 0; i < brew.length; i++){
+          if(favs.includes(brew[i]["brewery_id"])){
+            brew[i]["fav"] = 1;
+            brew[i]["user_id"] = user_id;
+          }else{
+            brew[i]["fav"] = 0;
+            brew[i]["user_id"] = user_id;
+          };
+        };
+        setBeers(brew);
+      }else{
+        setBeers(brew);
+      };
+    };
 
   useEffect(() => {
     async function getBeers() {
+      let favData = [];
+      const favUrl = `${process.env.REACT_APP_BREWERIES_AND_BEERS_API_HOST}/favorites/beers`
+      const favResponse = await fetch(favUrl, { method: "GET", credentials: "include" });
+      if (favResponse.ok) {
+        const data = await favResponse.json();
+        favData = Array.from(data);
+        
+      };
       const url = `${process.env.REACT_APP_BREWERIES_AND_BEERS_API_HOST}/beers`
       const response = await fetch(url);
+      let formattedData = [];
       if (response.ok) {
         const data = await response.json();
-        let formattedData = [];
         data.beers.map((obj) => {
           return formattedData.push(obj);
         });
+        addFavsToBreweries(formattedData, favData);
+      }else{
         setBeers(formattedData);
-      }
-    }
+      };
+    };
+
     getBeers();
   }, [])
 
